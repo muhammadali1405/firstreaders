dae34e0fb58be7cee2a849a4f30760dc1df1ac61
ali <ali.nazarudeen14@gmail.com>
2020-07-07 13:58:42 +0530

    made on seperate js file
    mooved the navbar modification to anther js file

---
+++ index.js
@@ -0,0 +1,34 @@
+//first code
+
+let isTransparent = true;
+const nav = document.getElementById("myHeader");                                              
+window.addEventListener('scroll', event => {
+    if (window.pageYOffset > 70 && isTransparent) 
+     {
+     nav.classList.add('_fixed');
+     isTransparent = false;
+     }
+    if (window.pageYOffset <= 70 && !isTransparent) 
+     {
+     nav.classList.remove('_fixed');
+     isTransparent = true;
+     }
+    });
+
+//second code
+
+const nav1 = document.getElementById("logo_main");
+window.addEventListener('scroll', event => 
+   {
+    if (window.pageYOffset > 70 ) 
+     {
+     nav1.classList.add('_fixed');
+     }
+    if (window.pageYOffset <= 70) 
+     {
+     nav1.classList.remove('_fixed');
+     }
+    });
+    
+/*the above two scripts are used to add a new class, so that  when the page is scrolled down
+     the header gets a new style and it will be animated to a small size and more space to view content */
